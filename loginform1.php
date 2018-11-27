6ffdf2de8d20850c575c7462653f1f80ab13e79e
faraxxx <33971033+faraxxx@users.noreply.github.com>
2018-11-28 04:04:36 +0500

    php with sql login form
    login forms

---
+++ loginform1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form method="post" action="loginfrom2.php">
+Name:<input type="text" name="name" /><br />
+Password:<input type="password" name="pwd" /><br />
+<input type="submit" />
+</form>
+</body>
+</html>
